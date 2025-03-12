caf31f5550f537b01e4c0fbfe96b9b4d079d4775
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-03-12 15:20:28 +0100

    ADDED THE FIRST api anrop

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const searchBtn = document.getElementById("search-btn")
+const moviesResult = document.getElementById("movies-result")
 
 
 const displayMovies = async () => {
-    const res = await fetch(`http://www.omdbapi.com/?apikey=217fbd44&t=titanic`) 
+    const res = await fetch(`http://www.omdbapi.com/?apikey=217fbd44&s=hor`) 
     const data = await res.json()
     console.log(data)
+    const returnedmovies = data.Search.map(oneMovie => {
+       return `
+        <div>
+        <h2>${oneMovie.Title}</h2>
+        
+        </div>
+        `
+    }).join("")
+    moviesResult.innerHTML = returnedmovies
 }
 searchBtn.addEventListener("click", displayMovies)
+
+{/* <p>${oneMovie.Ratings[0].Value}</p> */}
