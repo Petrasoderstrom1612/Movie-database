a12dce9302600529762b73c452a1157b7c89e310
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-03-13 13:10:26 +0100

    added the second API anrop to get all the details about each movie

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
+// https://www.omdbapi.com/ documentation
+// http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchedWord} all movies with the key word (not much data about each film)
+// https://www.omdbapi.com/?apikey=${API_KEY}&t=ahoj specific movie by title
+// https://www.omdbapi.com/?apikey=${API_KEY}&i=tt12968102 specific movie by imdbID
+
 import { API_KEY } from "./config.js";
 const searchBtn = document.getElementById("search-btn")
 const moviesResult = document.getElementById("movies-result")
 
-let searchedWord = "hor"
+let searchedWord = "ahoj"
+let arrOfSearchedMovies = []
+let searchedMoviesHTML = ""
 
 
 const displayMovies = async () => {
     const res = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchedWord}`) 
     const data = await res.json()
     console.log(data)
-    const returnedmovies = data.Search.map(oneMovie => {
-       return `
-        <div>
-        <h2>${oneMovie.Title}</h2>
+    let moviesArr = data.Search
+    arrOfSearchedMovies = moviesArr.map(oneMovie => oneMovie.imdbID);
+    moviesResult.innerHTML = arrOfSearchedMovies
+    console.log(arrOfSearchedMovies)
+
+    for (let i=0; i< arrOfSearchedMovies.length; i++){
+        console.log(arrOfSearchedMovies[i])
+
+        const res = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${arrOfSearchedMovies[i]}`);
+        const data = await res.json();
+            console.log("the individual movie", data)
+            console.log(data.Title, data.imdbRating)
+            searchedMoviesHTML += `
+            <p>${data.Title}</p>
+            <p>${data.imdbRating}</p>
+            `
         
-        </div>
-        `
-    }).join("")
-    moviesResult.innerHTML = returnedmovies
+    }
+    moviesResult.innerHTML = searchedMoviesHTML
+     
+
+
+
 }
 searchBtn.addEventListener("click", displayMovies)
 
